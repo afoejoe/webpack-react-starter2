a33d6e7f84e1a8ef1027f5c33a0929b008b9fc4a
afoejoe <afoejoe@gmail.com>
2024-02-20 10:01:41 +0300

    add local packages

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,66 +1,65 @@
 import path from 'path';
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-    mode: 'development',
-    entry: "./src/index.tsx",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true
-    },
-    devServer: {
-        static: './dist',
-        port: 8000,
-        hot: true
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            // {
-            //     test: /\.tsx?$/,
-            //     use: 'ts-loader',
-            //     exclude: /node_modules/,
-            // },
-            {
-                test: /\.css$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    // MiniCssExtractPlugin.loader,
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader"
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true
+  },
+  devServer: {
+    static: './dist',
+    port: 8000,
+    hot: true
+  },
+  optimization: {
+    runtimeChunk: 'single'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          // MiniCssExtractPlugin.loader,
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader'
         ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            title: "Reeact App",
-            template: "./src/index.html"
-        })
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      }
     ]
-}
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Reeact App',
+      template: './src/index.html'
+    })
+  ]
+};
+
+export default config;
